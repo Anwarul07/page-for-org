7e6cb97be037f5b5890281fdbbc790e721bc69b4
Anwarul07 <mdanwarul064@gmail.com>
2024-03-28 12:47:19 +0530

    Uodate hamberger

--- org.js
+++ org.js
@@ -1,1 +1,16 @@
-console.log("Welcome to the my org")
+console.log("Welcome to the my org");
+document.querySelector(".cross").style.display = "none";
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".sidebar").classList.toggle("sidebargo");
+
+    if (document.querySelector(".sidebar").classList.contains("sidebargo")) {
+        document.querySelector(".cross").style.display = "none";
+        document.querySelector(".ham").style.display = "inline";
+    }
+    else {
+        document.querySelector(".ham").style.display = "none";
+        setTimeout(() => {
+            document.querySelector(".cross").style.display = "inline";
+        }, 400);
+    }
+});
